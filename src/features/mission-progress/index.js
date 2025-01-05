8eb9115cbba11d039a747dc4780f03473e7394c0
seha <hansaemj20@gmail.com>
2025-01-05 18:01:24 +0900

    feature/level2 : 요구사항 3 - 질문, 댓글 수를 경험치에 반영하기

--- src/features/mission-progress/index.js
+++ src/features/mission-progress/index.js
@@ -1,15 +1,29 @@
-import { UserLevel } from './ui/user-level'
-import { UserExperience } from './ui/user-experience'
-import { UserActivity } from './ui/user-activity'
-import './mission-progress.css';
+import { UserLevel } from "./ui/user-level";
+import { UserExperience } from "./ui/user-experience";
+import { UserActivity } from "./ui/user-activity";
+import "./mission-progress.css";
+import { useEffect, useState } from "react";
+
+const calculateExperience = (base, answer, comment) => {
+  const tmp = base + (answer + comment) / 10;
+  return parseInt(tmp);
+};
 
 export const Progress = () => {
+  const [answerCount, setAnswerCount] = useState(12);
+  const [commentCount, setCommentCount] = useState(34);
+  const [experience, setExperience] = useState(10);
+
+  useEffect(() => {
+    setExperience(calculateExperience(experience, answerCount, commentCount));
+  }, [answerCount, commentCount]);
+
   return (
     <div className="mission-progress">
       <UserLevel />
-      <UserExperience />
-      <UserActivity label={"질문에 답한 수"} count={12}/>
-      <UserActivity label={"댓글 단 수"} count={34}/>
+      <UserExperience exp={Math.min(experience, 100)} />
+      <UserActivity label={"질문에 답한 수"} count={answerCount} />
+      <UserActivity label={"댓글 단 수"} count={commentCount} />
     </div>
   );
-}
+};
