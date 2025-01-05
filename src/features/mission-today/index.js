299c9753090eee0d46a7e77d1915eb2e853791da
seha <hansaemj20@gmail.com>
2025-01-05 18:40:33 +0900

    feature/level2 : 요구사항 4 - 미션 박스 클릭하면 색상 토글하기

--- src/features/mission-today/index.js
+++ src/features/mission-today/index.js
@@ -1,19 +1,35 @@
 import "./mission-today.css";
 import { MissionItem } from "./ui/mission-item";
+import { useState } from "react";
 
 export const Today = () => {
+  const [completions, setCompletion] = useState([true, false, false, true]);
+  const missions = [
+    "출석체크 미션",
+    "주제 답글 미션",
+    "공부 인증하기 미션",
+    "질문에 답변해보기",
+  ];
+
+  const handleToggle = (index) => {
+    const updatedCompletions = completions.map((task, i) =>
+      i === index ? !task : task
+    );
+    setCompletion(updatedCompletions);
+  };
+
   return (
     <div className="missionToday">
       <h3>오늘의 미션을 수행하고 포인트를 얻어보세요!</h3>
       <div className="missionItems">
-        <MissionItem mission={"출석체크 미션"} isCompleted={true} point={10} />
-        <MissionItem mission={"주제 답글 미션"} isCompleted={false} />
-        <MissionItem mission={"공부 인증하기 미션"} isCompleted={false} />
-        <MissionItem
-          mission={"질문에 답변해보기"}
-          isCompleted={true}
-          point={10}
-        />
+        {completions.map((isCompleted, index) => (
+          <MissionItem
+            key={index}
+            mission={missions[index]}
+            isCompleted={isCompleted}
+            onToggle={() => handleToggle(index)}
+          />
+        ))}
       </div>
     </div>
   );
