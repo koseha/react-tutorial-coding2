37a9980145355ab9b6bfb77f533de302b731dd98
seha <hansaemj20@gmail.com>
2025-01-05 19:15:47 +0900

    feature/level2 : 요구사항 5 - 미션 토글 시 점수 반영

--- src/features/mission-progress/index.js
+++ src/features/mission-progress/index.js
@@ -2,22 +2,8 @@ import { UserLevel } from "./ui/user-level";
 import { UserExperience } from "./ui/user-experience";
 import { UserActivity } from "./ui/user-activity";
 import "./mission-progress.css";
-import { useEffect, useState } from "react";
 
-const calculateExperience = (base, answer, comment) => {
-  const tmp = base + (answer + comment) / 10;
-  return parseInt(tmp);
-};
-
-export const Progress = () => {
-  const [answerCount, setAnswerCount] = useState(12);
-  const [commentCount, setCommentCount] = useState(34);
-  const [experience, setExperience] = useState(10);
-
-  useEffect(() => {
-    setExperience(calculateExperience(experience, answerCount, commentCount));
-  }, [answerCount, commentCount]);
-
+export const Progress = ({ experience, answerCount, commentCount }) => {
   return (
     <div className="mission-progress">
       <UserLevel />
